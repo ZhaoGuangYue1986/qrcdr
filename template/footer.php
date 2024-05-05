87ec3dda836823703fc6d5bf72382e74dc2227ff
ZhaoGuangYue1986 <jeky_zhao@qq.com>
2024-05-05 08:24:56 +0800

    Update footer.php

--- template/footer.php
+++ template/footer.php
@@ -8,6 +8,114 @@
       </div>
     </div>
     <hr>
+    <div class="wp cl">
+<div class="GzList" style="margin-top:10px;width:1180px;">
+    
+    <ul class="hover">
+<li><a href="http://tools.jike.in" target="_blank" style="color:#0000FF;">程序员工具箱</a></li>
+
+<li><a href="https://curl.qcloud.com/QOV8LzW2" target="_blank" style="color:#0000FF;">1核2G服务器首年99元起</a></li>
+
+<li><a href="https://curl.qcloud.com/Tpf4bi7B" target="_blank" style="color:#0000FF;">腾讯云新客户专享大礼包</a></li>
+
+<li><a href="https://curl.qcloud.com/mYrPnBLR" target="_blank" style="color:#0000FF;">【腾讯云】ElasticSearch</a></li>
+
+<li><a href="https://curl.qcloud.com/8OwKC727" target="_blank" style="color:#0000FF;">【腾讯云】即时通信特惠99</a></li>
+
+<li><a href="https://curl.qcloud.com/lM9DsSeJ	" target="_blank" style="color:#0000FF;">特惠秒杀1核2G云服务器38</a></li>
+
+<li><a href="http://shenzhenjia.net" target="_blank" style="color:#0000FF;">深圳家论坛</a></li>
+
+<li><a href="http://shenghuobao.net" target="_blank" style="color:#0000FF;">生活宝-关注生活，发现美</a></li>
+
+<li><a href="http://xstack.us" target="_blank" style="color:#0000FF;">xstack交流社区</a></li>
+
+<li><a href="http://overstack.in" target="_blank" style="color:#0000FF;">overstack社区</a></li>
+
+<li><a href="https://ogeek.cn/search.php?mod=portal&amp;searchid=13&amp;searchsubmit=yes&amp;kw=Java+%E4%BB%A3%E7%A0%81%E7%A4%BA%E4%BE%8B" target="_blank" style="color:#0000FF;">Java代码示例</a></li>
+
+<li><a href="https://ogeek.cn/search.php?mod=portal&amp;searchid=14&amp;searchsubmit=yes&amp;kw=PHP+%E4%BB%A3%E7%A0%81%E7%A4%BA%E4%BE%8B" target="_blank" style="color:#0000FF;">PHP代码示例</a></li>
+
+<li><a href="http://qrcode.jike.in" target="_blank" style="color:#0000FF;">网址转二维码</a></li>
+
+<li><a href="http://url.jike.in" target="_blank" style="color:#0000FF;">网址缩短</a></li>
+
+<li><a href="http://t.jike.in" target="_blank" style="color:#0000FF;">网址转二维码</a></li>
+
+<li><a href="http://qr.jike.in/" target="_blank" style="color:#0000FF;">网址转二维码</a></li>
+
+<li><a href="http://qrcode.16892.net" target="_blank" style="color:#0000FF;">qrcode.16892.net</a></li>
+
+<li><a href="http://qrcode.55276.net" target="_blank" style="color:#0000FF;">qrcode.55276.net</a></li>
+
+<li><a href="http://qrcode.devdocs.uk" target="_blank" style="color:#0000FF;">qrcode.devdocs.uk</a></li>
+
+<li><a href="http://qrcode.mlink.in" target="_blank" style="color:#0000FF;">qrcode.mlink.in</a></li>
+
+<li><a href="http://qrcode.ogeek.cn" target="_blank" style="color:#0000FF;">qrcode.ogeek.cn</a></li>
+
+<li><a href="http://qrcode.ostack.cn" target="_blank" style="color:#0000FF;">qrcode.ostack.cn</a></li>
+
+<li><a href="http://qrcode.shenghuobao.net" target="_blank" style="color:#0000FF;">qrcode.shenghuobao.net</a></li>
+
+<li><a href="http://qrcode.shenzhenjia.cn" target="_blank" style="color:#0000FF;">qrcode.shenzhenjia.cn</a></li>
+
+<li><a href="http://qrcode.sqlite.in" target="_blank" style="color:#0000FF;">qrcode.sqlite.in</a></li>
+
+<li><a href="http://qrcode.vigge.cn" target="_blank" style="color:#0000FF;">qrcode.vigge.cn</a></li>
+
+<li><a href="http://qrcode.vigge.net" target="_blank" style="color:#0000FF;">qrcode.vigge.net</a></li>
+
+<li><a href="http://qrcode.vigges.net" target="_blank" style="color:#0000FF;">qrcode.vigges.net</a></li>
+
+<li><a href="http://qrcode.wujigu.com" target="_blank" style="color:#0000FF;">qrcode.wujigu.com</a></li>
+
+<li><a href="http://qrcode.xstack.ru" target="_blank" style="color:#0000FF;">qrcode.xstack.ru</a></li>
+
+<li><a href="http://tools.16892.net" target="_blank" style="color:#0000FF;">tools.16892.net</a></li>
+
+<li><a href="http://tools.55276.net" target="_blank" style="color:#0000FF;">tools.55276.net</a></li>
+
+<li><a href="http://tools.devdocs.uk" target="_blank" style="color:#0000FF;">tools.devdocs.uk</a></li>
+
+<li><a href="http://tools.mlink.in" target="_blank" style="color:#0000FF;">tools.mlink.in</a></li>
+
+<li><a href="http://tools.ogeek.cn" target="_blank" style="color:#0000FF;">tools.ogeek.cn</a></li>
+
+<li><a href="http://tools.ostack.cn" target="_blank" style="color:#0000FF;">tools.ostack.cn</a></li>
+
+<li><a href="http://tools.ostack.cn" target="_blank" style="color:#0000FF;">tools.ostack.cn</a></li>
+
+<li><a href="http://tools.shenghuobao.net" target="_blank" style="color:#0000FF;">tools.shenghuobao.net</a></li>
+
+<li><a href="http://tools.shenzhenjia.cn" target="_blank" style="color:#0000FF;">tools.shenzhenjia.cn</a></li>
+
+<li><a href="http://tools.sqlite.in" target="_blank" style="color:#0000FF;">tools.sqlite.in</a></li>
+
+<li><a href="http://tools.vigge.cn" target="_blank" style="color:#0000FF;">tools.vigge.cn</a></li>
+
+<li><a href="http://tools.vigge.net" target="_blank" style="color:#0000FF;">tools.vigge.net</a></li>
+
+<li><a href="http://tools.vigges.net" target="_blank" style="color:#0000FF;">tools.vigges.net</a></li>
+
+<li><a href="http://tools.wujigu.com" target="_blank" style="color:#0000FF;">tools.wujigu.com</a></li>
+
+<li><a href="http://tools.xstack.ru" target="_blank" style="color:#0000FF;">tools.xstack.ru</a></li>
+
+<li><a href="plugin.php?id=iplus_gezi&amp;op=buy" onclick="showWindow('login', this.href);return false;" style="color:#333333;">点击自助购买广告位</a></li>
+
+<li><a href="plugin.php?id=iplus_gezi&amp;op=buy" onclick="showWindow('login', this.href);return false;" style="color:#333333;">点击自助购买广告位</a></li>
+
+<li><a href="plugin.php?id=iplus_gezi&amp;op=buy" onclick="showWindow('login', this.href);return false;" style="color:#333333;">点击自助购买广告位</a></li>
+
+<li><a href="plugin.php?id=iplus_gezi&amp;op=buy" onclick="showWindow('login', this.href);return false;" style="color:#333333;">点击自助购买广告位</a></li>
+
+<li><a href="plugin.php?id=iplus_gezi&amp;op=buy" onclick="showWindow('login', this.href);return false;" style="color:#333333;">点击自助购买广告位</a></li>
+		
+    </ul>
+    
+</div>
+</div>	  
     <div class="row py-2 small mb-3">
 		<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 		<!-- 底部广告长方形广告 -->
